ce0c5d3c4cb89111f225213ef3072b98318a36df
naotarian <naonao.070294@gmail.com>
2022-05-24 23:40:09 +0900

    firestore連携まで

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import './App.css'
+import { db } from './firebase'
+import { collection, getDocs, getDoc, doc } from 'firebase/firestore'
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const usersCollectionRef = collection(db, 'users');
+    getDocs(usersCollectionRef).then((querySnapshot) => {
+      setUsers(
+        querySnapshot.docs.map((docu) => ({ ...docu.data(), id: docu.id }))
+      )
+    });
+    const userDocument = doc(db, 'users', '1');
+    getDoc(userDocument).then((documentSnapshot) => {
+      console.log(documentSnapshot.data());
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.map((user, index) => (
+        <div key={index}>
+          <p>{user.id}</p>
+          <p>{user.email}</p>
+          <p>{user.name}</p>
+        </div>
+
+      ))}
     </div>
   );
 }
